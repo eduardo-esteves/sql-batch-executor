33b09d54b86fad41d7e56b4898bb61786b2dada5
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2024-05-16 20:37:53 -0300

    Improved output logs

--- update_db.py
+++ update_db.py
@@ -19,13 +19,23 @@ config = {
 BASE_DIR = Path(__file__).resolve().parent
 
 
-def execute_sql_file(cursor, file_path):
+def execute_sql_file(cursor: mysql.connector.cursor, file_path: str) -> None:
+    """
+    Reads an SQL fiel and executes its commands using the provided cursor.
+
+    Args:
+        cursor (mysql.connector.cursor): The database cursor.
+        file_path (str): The path to the SQL file.
+
+    @author Eduardo Esteves
+    """
     with open(file_path, 'r', encoding='utf-8') as file:
         sql_script = file.read()
 
     commands = []
     delimiter = ";"
     command = ""
+    sql_file = Path(file_path).name
 
     for line in sql_script.splitlines():
         if line.startswith("DELIMITER"):
@@ -48,10 +58,24 @@ def execute_sql_file(cursor, file_path):
             if command.strip():
                 cursor.execute(command)
         except mysql.connector.Error as err:
-            print(f"Error executing command: {command}\nError: {err}")
+            match err.errno:
+                case 1060:
+                    print(f"\nWarning: {err.errno} {err.sqlstate} {err.msg} in {sql_file}\n")
+                case _:
+                    print(f"\nError executing command: {command}\nError: {err} in {sql_file}\n")
 
 
 def main():
+    """
+    Main function to execute SQL scripts from a specified directory in order.
+
+    - Connects to the database using the configuration from enviroment variables.
+    - Lists and sorts SQL files in the specified directory.
+    - Executes each SQL file in order.
+
+    @author Eduardo Esteves
+    """
+
     # Path to the folder with SQL files
     path = Path(BASE_DIR, 'database')
 
